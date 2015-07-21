b3dfea12046fedebc551a6a5dd937bbaa0149284
Oswald De Riemaecker <oswald@continuousphp.com>
2015-07-22 00:38:52 +0200

    Added supported platforms

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description      'Installs/Configures aws-codedeploy-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'ubuntu', '= 14.02'
+supports 'centos', '= 7'
+supports 'fedora', '= 21'
+
 depends 'ohai'
 depends 'build-essential'
 depends 'rbenv'
