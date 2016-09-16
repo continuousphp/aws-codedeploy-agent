b0fb63f722f84f3feeedbc2d626e8d9db99b78c1
Oswald De Riemaecker <oswald@continuousphp.com>
2016-09-16 14:19:30 +0200

    clean up cookbook from version 0.5.1 + README.md update

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
 default['aws-codedeploy-agent']['rbenv_ruby-version'] = '2.1.5'
-default['aws-codedeploy-agent']['rbenv_ruby-version-fedora'] = 'ruby-2.1.5'
-default['aws-codedeploy-agent']['ruby-version'] = 'ruby-2.1.5'
