d17d73b1f09fd3a4bf07e879731175be48afda39
Thomas Harvey <tom@alush.co.uk>
2016-10-20 21:02:07 +0100

    filtering the grep process from the process checking test

--- test/integration/aws-codedeploy-agent/serverspec/default_spec.rb
+++ test/integration/aws-codedeploy-agent/serverspec/default_spec.rb
@@ -6,6 +6,6 @@ describe service('codedeploy-agent') do
   it { should be_enabled }
 end
 
-describe command('ps -ax | grep "codedeploy-agent: InstanceAgent"') do
+describe command('ps -ax | grep "codedeploy-agent: InstanceAgent" | grep -v grep') do
   its(:stdout) { should match /codedeploy-agent: InstanceAgent/ }
 end
