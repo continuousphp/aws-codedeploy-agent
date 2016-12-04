037e7db121a9f46c6846600a17b6176e9e2caa2e
Tom Harvey <tom@alush.co.uk>
2016-12-04 22:24:59 +0000

    Use tagged version of code-deploy-agent

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['aws-codedeploy-agent']['rbenv_ruby-version'] = '2.1.5'
 default['aws-codedeploy-agent']['aws_sdk_core-version'] = '2.6.11'
-default['aws-codedeploy-agent']['aws_codedeploy_agent-version'] = 'master'
+default['aws-codedeploy-agent']['aws_codedeploy_agent-version'] = 'v1.0-1045'
